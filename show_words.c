203c6b7872417475fc4729c90fe026b6ad2b0c53
hcy0157 <harnm0157@gmail.com>
2024-05-19 21:56:07 +0900

    add words in show_words file

--- show_words.c
+++ show_words.c
@@ -3,6 +3,37 @@
 #include <stdlib.h>
 #include "wordquiz.h"
 
+void command(char *wordbook){
+	printf("1. Add other words in %s\n", wordbook);
+	printf("2. Edit words in %s\n", wordbook);
+	printf("3. Exit\n");
+	printf("\n") ;
+	printf(">") ;
+}
+
+void add_words(char *filepath, FILE *fp, char *wordbook){
+	
+	fp = freopen(filepath, "a+", fp);
+	if (fp == NULL) {
+		printf("Failed to open file\n");
+		return;
+	}
+
+	char meaning[40]; 
+	char words[20];
+	
+	printf("Input words : ");
+	scanf("%s", words);
+	printf("Input its meaning : ");
+	scanf(" %[^\n]", meaning);
+	
+	fprintf(fp, "%s\" \"%s\n", words, meaning);
+}
+
+void edit_words(FILE *fp, char *wordbook){
+	printf("this is edit words\n");
+}
+
 void show_words ()
 {
 	char wordbook[128] ;
@@ -25,11 +56,35 @@ void show_words ()
 		strtok(NULL, "\"") ;
 		char * meaning = strtok(NULL, "\"") ;
 
-		printf("  %s : %s\n", word, meaning) ;
+		printf("  %s : %s\n", word, meaning);
 
 		free(line);
 	}
 	printf("  -----\n\n") ;
+
+	int cmd;
+	do{
+		command(wordbook);
+		scanf("%d", &cmd);
+		switch (cmd){
+			case 1: {
+				add_words(filepath,fp, wordbook);
+				break;
+			}
+			case 2:{
+				edit_words(fp, wordbook);
+				break;
+			}
+			case 3: {
+				break;
+			}
+			default:{
+				printf("It is an incorrect input\n");
+				break;
+			}
+		}
+
+	}while(cmd != 3);
 
 	fclose(fp) ;
 }
