b5ec4ec6e9c50730f5772365b7f014bc352b7265
LYH <yh@chungbuk.ac.kr>
2024-05-24 13:47:31 +0900

    add shuffle words fuction in run_test

--- run_test.c
+++ run_test.c
@@ -14,15 +14,22 @@ void run_test ()
 
 	sprintf(filepath, "wordbooks/%s", wordbook) ;
 
-	FILE * fp = fopen(filepath, "r") ;
+	// FILE * fp = fopen(filepath, "r") ;//orginal
+	int n_words;
+    Word *words = load_words(filepath, &n_words);
+    if (words == NULL) {
+        return;
+    }
+
+    shuffle_words(words, n_words);
 
 	printf("\n-----\n") ;
 
 	int n_questions = 0 ;
 	int n_correct = 0 ; 
 
-	char * line ;
-	while (line = read_a_line(fp)) {
+	/*char * line ;
+	 while (line = read_a_line(fp)) {
 		char * word = strtok(line, "\"") ;
 		strtok(NULL, "\"") ;
 		char * meaning = strtok(NULL, "\"") ;
@@ -49,6 +56,28 @@ void run_test ()
 
 	printf("-----\n\n") ;
 
-	fclose(fp) ;
+	fclose(fp) ; *///original
+	for (int i = 0; i < n_words; i++) {
+        printf("Q. %s\n", words[i].meaning);
+        printf("?  ");
+
+        char answer[128];
+        scanf("%s", answer);
+
+        if (strcmp(answer, words[i].word) == 0) {
+            printf("- correct\n");
+            n_correct++;
+        } else {
+            printf("- wrong: %s\n", words[i].word);
+        }
+
+        n_questions++;
+    }
+
+    printf("(%d/%d)\n", n_correct, n_questions);
+
+    printf("-----\n\n");
+
+    free(words);
 }
 
